e2c7594ce1ddbd6a54deab0fffba2418833d16b0
lilBunnyRabbit <andraz_ms@hotmail.com>
2023-03-13 02:43:37 +0100

    Fixes router to work with Github pages

--- src/router.tsx
+++ src/router.tsx
@@ -1,55 +1,60 @@
-import { createBrowserRouter, Navigate } from "react-router-dom";
+import { createHashRouter, Navigate } from "react-router-dom";
 import { AppLayout } from "./layouts/AppLayout";
 import { IndexView } from "./views/IndexView";
-import { YearsView } from "./views/years/YearsView";
-import { MonthsView } from "./views/years/MonthsView";
 import { DaysView } from "./views/years/DaysView";
 import { DayView } from "./views/years/DayView";
+import { MonthsView } from "./views/years/MonthsView";
+import { YearsView } from "./views/years/YearsView";
 
-export const router = createBrowserRouter([
+export const router = createHashRouter(
+  [
+    {
+      path: "/",
+      element: <AppLayout />,
+      children: [
+        {
+          index: true,
+          element: <IndexView />,
+        },
+        {
+          path: "years",
+          children: [
+            {
+              index: true,
+              element: <YearsView />,
+            },
+            {
+              path: ":year/months",
+              children: [
+                {
+                  index: true,
+                  element: <MonthsView />,
+                },
+                {
+                  path: ":month/days",
+                  children: [
+                    {
+                      index: true,
+                      element: <DaysView />,
+                    },
+                    {
+                      path: ":day",
+                      element: <DayView />,
+                    },
+                  ],
+                },
+              ],
+            },
+          ],
+        },
+        {
+          path: "*",
+          element: <Navigate to="/years" />,
+        },
+      ],
+    },
+  ],
   {
-    path: "/",
-    element: <AppLayout />,
-    children: [
-      {
-        index: true,
-        element: <IndexView />,
-      },
-      {
-        path: "years",
-        children: [
-          {
-            index: true,
-            element: <YearsView />,
-          },
-          {
-            path: ":year/months",
-            children: [
-              {
-                index: true,
-                element: <MonthsView />,
-              },
-              {
-                path: ":month/days",
-                children: [
-                  {
-                    index: true,
-                    element: <DaysView />,
-                  },
-                  {
-                    path: ":day",
-                    element: <DayView />,
-                  },
-                ],
-              },
-            ],
-          },
-        ],
-      },
-      {
-        path: "*",
-        element: <Navigate to="/years" />,
-      },
-    ],
-  },
-]);
+    basename: "work-hours",
+  }
+);
